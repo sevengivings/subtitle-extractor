11397bcdfaf17745846f5e7a6e8acf099adea21f
Kiyoung Im <imkiyoung@gmail.com>
2023-08-17 11:54:45 +0900

    ver 2023-08-17 : added DeepL API translation 	modified:   locales/ko_KR/LC_MESSAGES/subtitle-extractor.mo 	modified:   locales/ko_KR/subtitle-extractor.po 	modified:   locales/subtitle-extractor.pot 	modified:   subtitle-extractor.py

--- subtitle-extractor.py
+++ subtitle-extractor.py
@@ -1,4 +1,5 @@
 # Extract subtitle from video, save as .docx for translating and convert to .srt
+# https://github.com/sevengivings/subtitle-extractor
 
 # Requires:
 # - stable-ts (https://github.com/jianfch/stable-ts) (pip install stable-ts)
@@ -6,6 +7,7 @@
 # - torch + cuda
 # - ffmpeg.exe (https://www.ffmpeg.org/) for stable-ts and whisper 
 # - python-docx
+# - deepl (https://github.com/DeepLcom/deepl-python)
 
 # OS: Windows 10/11 
 
@@ -22,25 +24,29 @@ import gettext
 import locale 
 
 # stable-ts  
-def extract_audio_stable_whisper(model_name, device, audio_language, input_file_name, output_file_name): 
+def extract_audio_stable_whisper(model_name, device, use_condition_on_previous_text, use_demucs, use_vad, vad_threshold, is_mel_first, audio_language, input_file_name, output_file_name): 
     # Check if the file exists.
     if not os.path.exists(input_file_name):
         raise FileNotFoundError(f"The file {input_file_name} does not exist.")
+
+    print(f'condition_on_previous_text: {use_condition_on_previous_text}, demucs: {use_demucs}, vad: {use_vad}, vad_threshold: {vad_threshold}, mel_first: {is_mel_first}')
 
     # Extract the audio from the video.
     model = stable_whisper.load_model(model_name, device=device)
-    result = model.transcribe(verbose=True, word_timestamps=False, condition_on_previous_text=False,language=audio_language, audio=input_file_name)
+    result = model.transcribe(verbose=True, word_timestamps=False, condition_on_previous_text=use_condition_on_previous_text, \
+                                demucs=use_demucs, vad=use_vad, vad_threshold=vad_threshold, mel_first=is_mel_first, \
+                                language=audio_language if audio_language != "None" else None, audio=input_file_name)
     result.to_srt_vtt(output_file_name + ".srt", word_level=False)
 
 # Whisper 
-def extract_audio_whisper(model_name, device, audio_language, input_file_name):
+def extract_audio_whisper(model_name, device, use_condition_on_previous_text, audio_language, input_file_name):
     # Check if the file exists.
     if not os.path.exists(input_file_name):
         raise FileNotFoundError(f"The file {input_file_name} does not exist.")
-
+    print(f'condition_on_previous_text: {use_condition_on_previous_text}')
     model = whisper.load_model(model_name).to(device)
     temperature = tuple(np.arange(0, 1.0 + 1e-6, 0.2))  # copied from Whisper original code 
-    result = model.transcribe(input_file_name, temperature=temperature, verbose=True, word_timestamps=False, condition_on_previous_text=False, language=audio_language)
+    result = model.transcribe(input_file_name, temperature=temperature, verbose=True, word_timestamps=False, condition_on_previous_text=use_condition_on_previous_text, language=audio_language)
     output_dir = os.path.dirname(input_file_name)
     writer = get_writer("srt", output_dir)
     writer(result, input_file_name) 
@@ -50,7 +56,8 @@ def extract_audio_whisper(model_name, device, audio_language, input_file_name):
 def srt_split(input_file_name, skip_textlength):
     # Check if the file exists.
     if not os.path.exists(input_file_name):
-        raise FileNotFoundError(f"The file {input_file_name} does not exist.")
+        print(_("Error: No input file: ", input_file_name))
+        sys.exit(1)
 
     # Open the input file.
     with open(input_file_name, "r", encoding="utf-8") as f:
@@ -188,74 +195,15 @@ def join_srt_files(time_file, text_file, output_file):
         for i in range(len(time_data)):
             time_entry = time_data[i].strip()
             text_entry = text_data[i].strip()
-            output.write(f"{i+1}\n")
+            output.write(f"{i}\n")
             output.write(f"{time_entry}\n")
             output.write(f"{text_entry}\n")
             output.write("\n")
     
     print(_("Info: new srt file is saved"), output_file)
 
-if __name__ == "__main__":
-    appname = 'subtitle-extractor'
-    localedir = './locales'
-        
-    en_i18n = gettext.translation(appname, localedir, fallback=True, languages=['ko'])  # All messages are in Korean
-    en_i18n.install()
-
-    parser= argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("audio", type=str, help="audio/video file(s) to transcribe")
-    parser.add_argument("--model", default="medium", help="tiny, base, small, medium, large model to use")
-    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to use for PyTorch inference")
-    parser.add_argument("--audio_language", type=str, default="ja", help="language spoken in the audio, specify None to perform language detection")
-    parser.add_argument("--subtitle_language", type=str, default="ko", help="subtitle target language need only if you plan to use DeepL file translation manually")
-    parser.add_argument("--skip_textlength", type=int, default=1, help="skip short text in the subtitles, useful for removing meaningless words")
-    
-    args = parser.parse_args().__dict__
-    model_name: str = args.pop("model")
-    device: str = args.pop("device")
-    audio_language: str = args.pop("audio_language")
-    subtitle_language: str = args.pop("subtitle_language")
-    input_file_name: str = args.pop("audio")
-    skip_textlength: int = args.pop("skip_textlength")
-
-    print("subtitle-extractor : AI subtitle extraction and translation helper tool")
-    print("\nmodel:" + model_name + "\ndevice:" + device  + "\naudio language:" + audio_language + "\nsubtitle language:" + subtitle_language  + "\nigonore n characters:" + str(skip_textlength) + "\naudio:" + input_file_name)
-    print("\nPython version: " + sys.version)
-    print("Torch version: " + torch.__version__ + "\n")
-
-    if skip_textlength < 0:
-        skip_textlength = 0 
-
-    # create a new file with the same name as the input file, consider file name contains multiple '.' 
-    output_file_name = input_file_name.rsplit(".", 1)[0]
-
-    # to determine transcribing method, get user input 
-    # print ("Input the number of the transcribing method you want to use: 1 for Stable-ts, 2 for Whisper") 
-    number_selected = input(_("Input transcribing method. 1 for Stable-ts, 2 for Whisper: "))
-    try: 
-        number_selected = int(number_selected)
-    except ValueError:
-        print(_("Error: Enter numbers only"))
-        sys.exit(1)
-
-    # AI speech recognition  
-    # Check if the file exists
-    if not os.path.exists(output_file_name + ".srt"):           
-        if number_selected == 1:     
-            extract_audio_stable_whisper(model_name, device, audio_language, input_file_name, output_file_name)
-        elif number_selected == 2:
-            extract_audio_whisper(model_name, device, audio_language, input_file_name)
-        else: 
-            print(_("Error: Enter numbers only")) 
-            sys.exit(1)
-    else: 
-        print(_("Warning: File already exists"))
-
-    # Separate text and visuals in .SRT and save subtitle text as .docx for translation. 
-    # Removed short sentences and repeated subtitles.  
-    srt_split(output_file_name + ".srt", skip_textlength)
-
-    # Get the translated file name from console if not from DeepL file translation
+def translate_manually(output_file_name, subtitle_language): 
+   # Get the translated file name from console if not from DeepL file translation
     print(_("Info: You should translate .docx manullay using DeepL file translation, use "), output_file_name + ".docx")
     # input file name from console  
     file_name = input(_("Input another translated file name or press [Enter] to continue...(") + output_file_name + " " + subtitle_language + _(".docx will be used.): "))
@@ -265,7 +213,7 @@ if __name__ == "__main__":
         file_name = output_file_name + " " + subtitle_language + ".docx"
 
     if not os.path.exists(file_name): 
-        print(_("Error: File not found translated "), file_name)
+        print(_("Error: File not found"), file_name)
         sys.exit(1)
     
     # If input file is .txt, docx_to_txt is not used, instead .srt is used to refer to .txt file name.
@@ -276,6 +224,182 @@ if __name__ == "__main__":
         input(_("Press [Enter] to continue... or edit ") + file_name.rsplit(".", 1)[0] + ".txt ")
     
     text_file_name = file_name.rsplit(".", 1)[0]
+    
+    return text_file_name
+
+def translate_automatically(output_file_name, subtitle_language, deepl_api_key):
+    try: 
+        import deepl
+    except ModuleNotFoundError:
+        print(
+            "Please install deepl python package by running: pip install --upgrade deepl"
+        )
+        sys.exit(1)
+
+    translator = deepl.Translator(deepl_api_key)
+    input_path = output_file_name + ".docx"
+    output_path = output_file_name + " " + subtitle_language + ".docx"
+    try: 
+        target_lang = subtitle_language.upper()
+
+        # Using translate_document_from_filepath() with file paths 
+        translator.translate_document_from_filepath(
+            input_path,
+            output_path,
+            target_lang=target_lang
+        )
+
+        # Alternatively you can use translate_document() with file IO objects
+        with open(input_path, "rb") as in_file, open(output_path, "wb") as out_file:
+            translator.translate_document(
+                in_file,
+                out_file,
+                target_lang=target_lang 
+            )
+
+    except deepl.DocumentTranslationException as error:
+        # If an error occurs during document translation after the document was
+        # already uploaded, a DocumentTranslationException is raised. The
+        # document_handle property contains the document handle that may be used to
+        # later retrieve the document from the server, or contact DeepL support.
+        doc_id = error.document_handle.id
+        doc_key = error.document_handle.key
+        print(f"Error after uploading ${error}, id: ${doc_id} key: ${doc_key}")
+        print(_("DeepL API error: "), error)
+        sys.exit(1)    
+    except deepl.DeepLException as error:
+        # Errors during upload raise a DeepLException
+        print(error)    
+        print(_("DeepL API error: "), error)
+        sys.exit(1)    
+
+    usage = translator.get_usage()
+    if usage.any_limit_reached:
+        print('Translation limit reached.')
+    if usage.character.valid:
+        print(
+            f"Character usage: {usage.character.count} of {usage.character.limit}")
+    if usage.document.valid:
+        print(f"Document usage: {usage.document.count} of {usage.document.limit}")
+
+    docx_to_txt(output_path)
+
+    text_file_name = output_path.rsplit(".", 1)[0]
+
+    return text_file_name
+
+if __name__ == "__main__":
+    appname = 'subtitle-extractor'
+    localedir = './locales'
+        
+    en_i18n = gettext.translation(appname, localedir, fallback=True, languages=['ko'])  # All messages are in Korean
+    en_i18n.install()
+
+    print(_("\nAI subtitle transcription and DeepL translation helper tool\n"))
+
+    parser= argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("audio", type=str, help=_("audio/video file(s) to transcribe"))
+    parser.add_argument("--framework", default="none", help=_("name of the stable-ts or Whisper framework to use"))
+    parser.add_argument("--model", default="medium", help=_("tiny, base, small, medium, large model to use"))
+    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help=_("device to use for PyTorch inference"))
+    parser.add_argument("--language", type=str, default="None", help=_("language spoken in the audio, specify None to perform language detection"))
+    parser.add_argument("--subtitle_language", type=str, default="ko", help=_("subtitle target language need only if you plan to use DeepL file translation"))
+    parser.add_argument("--skip_textlength", type=int, default=1, help=_("skip short text in the subtitles, useful for removing meaningless words"))
+    parser.add_argument("--condition_on_previous_text", action='store_true',
+                        help=_("if True, provide the previous output of the model as a prompt for the next window; "
+                             "disabling may make the text inconsistent across windows, "
+                             "but the model becomes less prone to getting stuck in a failure loop"))
+    # stable_ts only 
+    parser.add_argument('--demucs', action='store_true',
+                        help=_('stable-ts only, whether to reprocess the audio track with Demucs to isolate vocals/remove noise; '
+                             'pip install demucs PySoundFile; '
+                             'Demucs official repo: https://github.com/facebookresearch/demucs'))
+    parser.add_argument('--vad', action='store_true',
+                        help=_('stable-ts only, whether to use Silero VAD to generate timestamp suppression mask; '
+                             'pip install silero; '
+                             'Official repo: https://github.com/snakers4/silero-vad'))
+    parser.add_argument('--vad_threshold', type=float, default=0.2,
+                        help=_('stable-ts only, threshold for detecting speech with Silero VAD. (Default: 0.2); '
+                             'low threshold reduces false positives for silence detection'))
+    parser.add_argument('--mel_first', action='store_true',
+                        help=_('stable-ts only, process entire audio track into log-Mel spectrogram first instead in chunks'
+                             'if audio is not transcribing properly compared to whisper, at the cost of more memory usage for long audio tracks'))
+
+    args = parser.parse_args().__dict__
+    framework: str = args.pop("framework")
+    model_name: str = args.pop("model")
+    device: str = args.pop("device")
+    audio_language: str = args.pop("language")
+    subtitle_language: str = args.pop("subtitle_language")
+    input_file_name: str = args.pop("audio")
+    skip_textlength: int = args.pop("skip_textlength")
+    use_condition_on_previous_text: bool = args.pop("condition_on_previous_text")
+
+    # stable_ts only
+    use_demucs: bool = args.pop("demucs")
+    use_vad: bool = args.pop("vad")
+    vad_threshold: float = args.pop("vad_threshold")
+    use_mel_first: bool = args.pop("mel_first")    
+
+    print("\nframework: " + framework)
+    print("model:" + model_name + "\ndevice:" + device  + "\naudio language:" + audio_language + "\nsubtitle language:" + subtitle_language  + "\nigonore n characters:" + str(skip_textlength) + "\naudio:" + input_file_name)
+    print("\nPython version: " + sys.version)
+    print("Torch version: " + torch.__version__ + "\n")
+
+    if skip_textlength < 0:
+        skip_textlength = 0 
+
+    if not os.path.exists(input_file_name): 
+        print(_("The input audio/video file does not exist: "), input_file_name)
+        sys.exit(1)
+
+    if audio_language == "None":
+        print(_("Info: --audio_language is not specified, performing automatic language detection"))
+
+    # create a new file with the same name as the input file, consider file name contains multiple '.' 
+    output_file_name = input_file_name.rsplit(".", 1)[0]
+
+    # slect transcribing method if not input by user 
+    if framework == "none":
+        # to determine transcribing method, get user input 
+        # print ("Input the number of the transcribing method you want to use: 1 for Stable-ts, 2 for Whisper") 
+        number_selected = input(_("Input transcribing method. 1 for Stable-ts, 2 for Whisper: "))
+        try: 
+            number_selected = int(number_selected)
+            if number_selected == 1:
+                framework = "stable-ts"
+            elif number_selected == 2:
+                framework = "whisper"
+        except ValueError:
+            print(_("Error: input numbers only"))
+            sys.exit(1)
+
+    # AI speech recognition  
+    # Check if the file exists
+    if not os.path.exists(output_file_name + ".srt"):           
+        if framework == "stable-ts":     
+            extract_audio_stable_whisper(model_name, device, use_condition_on_previous_text, use_demucs, use_vad, vad_threshold, use_mel_first, audio_language, input_file_name, output_file_name)
+        elif framework == "whisper":
+            extract_audio_whisper(model_name, device, use_condition_on_previous_text, audio_language, input_file_name)
+    else: 
+        print(_("Warning: File already exists"))
+
+    # Separate text and time sync data in .SRT and save subtitle text as .docx for translation. 
+    # Removed short sentences and repeated subtitles.  
+    srt_split(output_file_name + ".srt", skip_textlength)
+
+    # for DeepL API translation, check if DEEPL_API_KEY is set in environment variables.
+    # How to set DeepL API key in PowerShell : Set-Item -Path env:DEEPL_API_KEY -Value "your-id"
+    # How to remove DeepL API key in PowerShell : Remove-Item -Path env:DEEPL_API_KEY
+    text_file_name = ""
+    try: 
+        deepl_api_key = os.environ["DEEPL_API_KEY"]
+        print(_("Info: DeepL API key is set"))
+        print(_("Info: DeepL file translation is started, please wait"))
+        text_file_name = translate_automatically(output_file_name, subtitle_language, deepl_api_key)
+        print(_("Info: DeepL file translation is done"))        
+    except KeyError:
+        text_file_name = translate_manually(output_file_name, subtitle_language)
     
     # Change the name of original .srt
     os.rename(output_file_name + ".srt", output_file_name + "_original.srt")
@@ -294,14 +418,13 @@ if __name__ == "__main__":
 
     # If translated .txt is not provided, there is no ~ ko.docx or ~ ko.txt file.
     # If translated .txt is provided by the user, it is not deleted.
-    if not file_name.endswith(".txt"):
-        try: 
-            os.unlink (output_file_name + " " + subtitle_language + ".docx")
-            os.unlink (output_file_name + " " + subtitle_language + ".txt")
-            os.unlink (output_file_name + ".docx")
-        except FileNotFoundError: 
-            pass
-    
+    try: 
+        os.unlink (output_file_name + " " + subtitle_language + ".docx")
+        os.unlink (output_file_name + " " + subtitle_language + ".txt")
+        os.unlink (output_file_name + ".docx")
+    except FileNotFoundError: 
+        pass
+
     print(_("Done"))
 
     sys.exit(0)
